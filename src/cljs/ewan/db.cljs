d28e5d7ec0143175c24c5b9f0bb77874c814bdb9
Luke Gessler <ldg3fa@virginia.edu>
2018-03-15 19:48:03 -0500

    add deps and example for cljs-react-material-ui

--- src/cljs/ewan/db.cljs
+++ src/cljs/ewan/db.cljs
@@ -1,5 +1,4 @@
 (ns ewan.db
   (:require [re-frame.core :as re-frame]))
 
-(def default-db
-  {})
+(def default-db {})
--- src/cljs/ewan/views.cljs
+++ src/cljs/ewan/views.cljs
@@ -4,6 +4,11 @@
             [ewan.subs :as subs]
             [ewan.events :as events]
             [ewan.todos :as todos]
+            [cljsjs.material-ui]
+            [cljs-react-material-ui.core :refer [get-mui-theme color]]
+            [cljs-react-material-ui.reagent :as ui]
+            [cljs-react-material-ui.icons :as ic]
+            [reagent.core :as r]
             ))
 
 ;; home
@@ -51,6 +56,21 @@
 
 (defn main-panel []
   (let [active-panel (re-frame/subscribe [::subs/active-panel])]
-    [re-com/v-box
-     :height "100%"
-     :children [[panels @active-panel]]]))
+    [ui/mui-theme-provider
+     {:mui-theme (get-mui-theme
+                  {:palette {:text-color (color :green600)}})}
+     [:div
+      [ui/app-bar {:title "Title"
+                   :icon-element-right
+                   (r/as-element [ui/icon-button
+                                  (ic/action-account-balance-wallet)])}]
+      [ui/paper
+       [:div "Hello"]
+       [ui/mui-theme-provider
+        {:mui-theme (get-mui-theme {:palette {:text-color (color :blue200)}})}
+        [ui/raised-button {:label "Blue button"}]]
+       (ic/action-home {:color (color :grey600)})
+       [ui/raised-button {:label        "Click me"
+                          :icon         (ic/social-group)
+                          :on-click     #(println "clicked")}]]
+      [panels @active-panel]]]))
