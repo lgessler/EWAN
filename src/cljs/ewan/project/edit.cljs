4450a26e0a651ea2785b3bd8dfae6cc693e31b5e
Luke Gessler <ldg3fa@virginia.edu>
2018-04-12 18:01:21 -0500

    stub out media panel

--- src/cljs/ewan/project/edit.cljs
+++ src/cljs/ewan/project/edit.cljs
@@ -7,13 +7,24 @@
             [cljs-react-material-ui.icons :as ic]
             [reagent.core :as r]))
 
+(def ^:private default-db {::playback {}})
+
+;; ----------------------------------------------------------------------------
 ;; subs
+;; ----------------------------------------------------------------------------
 (rf/reg-sub
  ::current-project
  (fn [db _]
    (::current-project db)))
 
+(rf/reg-sub
+ ::playback
+ (fn [db _]
+   (::playback db)))
+
+;; ----------------------------------------------------------------------------
 ;; events
+;; ----------------------------------------------------------------------------
 ;; These two events are used to fetch a doc when entering #/project/:id
 (rf/reg-event-fx
  ::open-project
@@ -27,13 +38,59 @@
                        (if err
                          (throw err)
                          (rf/dispatch [::project-doc-fetched doc])))]}
-    :dispatch [:ewan.views/set-active-panel :project-edit-panel]}))
+    :dispatch-n [[:ewan.views/set-active-panel :project-edit-panel]
+                 [::init-db]]}))
 
 (rf/reg-event-db
  ::project-doc-fetched
  (fn [db [_ doc]]
-   (assoc db ::current-project doc)))
+   (assoc db ::current-project (js->clj doc :keywordize-keys true))))
+
+(rf/reg-event-db
+ ::init-db
+ (fn [db _]
+   (merge db default-db)))
+
+;; ----------------------------------------------------------------------------
+;; views
+;; ----------------------------------------------------------------------------
+
+(defn- video? [t]
+  (= "video" (first (clojure.string/split t #"/"))))
+
+(defn- media-panel-inner []
+  (let [!video (atom nil)
+        update
+        (fn [comp]
+          (let [{:keys [src play] :as playback} (r/props comp)
+                video @!video]
+            (js/console.log playback)
+            (set! (.-src video) src)
+            (if play
+              (.play video)
+              (.pause video))))]
+    (r/create-class
+     {:component-did-update update
+      :component-did-mount update
+      :reagent-render
+      (fn []
+        [:video {:ref #(reset! !video %)
+                 :width "480"
+                 :height "640"}])})))
+
+(defn- media-panel-outer []
+  (let [playback (rf/subscribe [::playback])]
+    (fn []
+      [media-panel-inner @playback])))
+
+(defn- video-panel []
+  (let [!ref (atom nil)]
+    (fn []
+      [:video {:ref (fn [com] (reset! !ref com))}])))
 
 (defn project-edit-panel-body [doc]
   (r/with-let [doc (rf/subscribe [::current-project])]
-    [:div (str @doc)]))
+    [:div (for [[_ file] (:_attachments @doc)]
+            (when (video? (:content_type file))
+              [:video {:src (.createObjectURL js/URL (:data file))
+                       :loop "loop"}]))]))
