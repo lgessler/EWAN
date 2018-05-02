39238c8c512cc2a442d6e14b5a78563fd6e98c30
Luke Gessler <ldg3fa@virginia.edu>
2018-05-02 16:24:51 -0500

    ewan.common (common.cljs) for shared functions e.g. for hiccup

---
+++ src/cljs/ewan/common.cljs
@@ -0,0 +1,18 @@
+(ns ewan.common
+  (:require [re-frame.core :as rf]))
+
+;; hiccup helpers
+(defn tag-name [hiccup] (first hiccup))
+(defn attrs [hiccup] (second hiccup))
+(defn first-child [hiccup] (nth hiccup 2))
+(defn children [hiccup] (drop 2 hiccup))
+
+(defn- simple-sub
+  "Convenience function for subs that just take a single value out of `db`"
+  ([kwd]
+   (rf/reg-sub kwd (fn [db _] (kwd db))))
+  ([name path]
+   (rf/reg-sub name (fn [db _] (get-in db path)))))
+
+
+
